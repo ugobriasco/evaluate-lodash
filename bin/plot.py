c09f8f630f198d9892144dd512c787210cb8f4b4
Ugo <ugo.briasco@sumup.com>
2021-08-31 15:39:21 +0200

    Reduce: Plot data using Histogram

--- bin/plot.py
+++ bin/plot.py
@@ -2,8 +2,6 @@ import json
 import pandas as pd
 import matplotlib.pyplot as plt
 
-
-
 # read file
 with open('./data/raw/evaluate-find.json', 'r') as myfile:
     raw_data=myfile.read()
@@ -19,10 +17,29 @@ fig = data.plot(\
     subplots=True, 
     figsize=(10, 10),
     bins=20,
-    title = "Find: Lodash vs Native [ms]"
+    title = "FIND: Lodash vs Native [ms]"
     )[0].get_figure()
 plt.tight_layout()
 fig.savefig('./data/plots/evaluate-find.png')
 
-# fig = data.plot(kind='bar',  figsize=(20, 16), fontsize=26).get_figure()
-# fig.savefig('./data/plots/evaluate-find.jpg')
+
+# read file
+with open('./data/raw/evaluate-reduce.json', 'r') as myfile:
+    raw_data=myfile.read()
+
+# parse file
+obj = json.loads(raw_data)
+
+data = pd.DataFrame({'lodash': obj['lodash'], 'native': obj['native']})
+#data.hist(figsize=(10,10),bins=10)
+
+fig = data.plot(\
+    kind='hist', 
+    subplots=True, 
+    figsize=(10, 10),
+    bins=20,
+    title = "REDUCE: Lodash vs Native [ms]"
+    )[0].get_figure()
+plt.tight_layout()
+fig.savefig('./data/plots/evaluate-reduce.png')
+
